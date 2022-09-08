9972854fb2dc673bf80253ce707bd76a8a697765
A-p-p-l-a <paulmahot33@gmail.com>
2022-09-09 01:59:15 +0200

    import project

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.json());
+
+const mongoose = require('mongoose');
+
+
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces');
+const Sauce = require('./models/Sauce');
+
+mongoose.connect('mongodb+srv://Appla:appla@cluster0.18r5vx8.mongodb.net/',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
